20e1d562c31434c2661cbada71fb05e2b0f15119
kosta709 <kosta777@gmail.com>
2021-07-23 09:52:07 +0300

    flannel changes

--- infra/vm.tf
+++ infra/vm.tf
@@ -62,7 +62,7 @@ resource google_compute_instance n0_0 {
 
   boot_disk {
     initialize_params {
-      image = "centos-cloud/centos-8"
+      image = "centos-cloud/centos-7"
     }
   }
 
@@ -103,5 +103,57 @@ resource google_compute_instance n1_0 {
   allow_stopping_for_update = true
 #   metadata_startup_script = <<-EOF
 #   EOF
+
+}
+
+resource google_compute_instance node_mn_0 {
+  name         = "kube-mn-n2-0"
+  machine_type = "n2-standard-4"
+  project = local.gcp_project
+  zone         = local.zone
+
+  boot_disk {
+    initialize_params {
+      image = "centos-cloud/centos-8"
+    }
+  }
+
+  can_ip_forward = true
+  network_interface {
+    network = "default"
+    access_config {
+
+    }    
+  }
+  network_interface {
+    subnetwork = google_compute_subnetwork.net0_subnet0.self_link
+    network_ip = "192.168.1.10"
+  }
+  network_interface {
+    subnetwork = google_compute_subnetwork.net1_subnet0.self_link
+    network_ip = "10.44.1.10"
+  }  
+
+ 
+  allow_stopping_for_update = true
+  metadata_startup_script = <<-EOF
+    echo "1 rt1" | sudo tee -a /etc/iproute2/rt_tables
+    ip route add 192.168.1.1 src 192.168.1.10 dev eth1 table rt1
+    ip route add default via 192.168.1.1 dev eth1 table rt1
+    ip rule add from 192.168.1.10/32 table rt1
+    ip rule add to 192.168.1.10/32 table rt1
+
+    echo "2 rt2" | sudo tee -a /etc/iproute2/rt_tables
+    ip route add 10.44.1.1 src 10.44.1.10 dev eth2 table rt2
+    ip route add default via 10.44.1.1 dev eth2 table rt2
+    ip rule add from 10.44.1.10/32 table rt2
+    ip rule add to 10.44.1.10/32 table rt2
+  EOF
+  
+  lifecycle {
+      ignore_changes = [
+          metadata_startup_script
+      ]
+  }
 
 }
